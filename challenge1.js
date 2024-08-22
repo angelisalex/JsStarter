61269cd7ce2a531b48b35ad232ff40a03ecc945a
angelisalex <alexandros.angelis.2017@gmail.com>
2024-08-22 12:26:38 +0300

    challenge 2

--- challenge1.js
+++ challenge1.js
@@ -22,3 +22,19 @@
 // scoreKoalas = calcAverage(23, 34, 27);
 // console.log(scoreDolphins, scoreKoalas);
 // checkWinner(scoreDolphins, scoreKoalas);
+function calcTip(bill) {
+  if (bill >= 50 && bill < 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills);
+console.log(tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
